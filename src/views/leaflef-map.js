2b3697e12b241365b0c35adcb7d47f160c0b7757
condef5 <davis.con.fab@gmail.com>
2019-06-18 09:20:09 -0500

    Add leaflet map

--- src/views/leaflef-map.js
+++ src/views/leaflef-map.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { Map, TileLayer, Marker } from "react-leaflet";
 
 function LeafletMap() {
-  return <h1>LeafletMap</h1>;
+  const [latitude, setLatitude] = React.useState(0);
+  const [longitude, setLongitude] = React.useState(0);
+
+  React.useEffect(() => {
+    const watchId = navigator.geolocation.watchPosition(({ coords }) => {
+      setLatitude(coords.latitude);
+      setLongitude(coords.longitude);
+    });
+
+    return () => navigator.geolocation.clearWatch(watchId);
+  }, [setLatitude, setLongitude]);
+
+  return (
+    <Map center={[latitude, longitude]} zoom={15}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors | Made for Codeable'
+      />
+      <Marker position={[latitude, longitude]} />
+    </Map>
+  );
 }
 
 export default LeafletMap;
