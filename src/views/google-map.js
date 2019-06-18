3f58d5d816d8c88c1b012aa03aa068c88b982728
condef5 <davis.con.fab@gmail.com>
2019-06-18 12:35:13 -0500

    Add google-maps

--- src/views/google-map.js
+++ src/views/google-map.js
@@ -1,8 +1,87 @@
-import React from "react";
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { Map, Marker, GoogleApiWrapper, InfoWindow } from "google-maps-react";
+class Container extends React.Component {
+  constructor(props) {
+    super(props);
 
-function GoogleMap() {
-  return <h1>GoogleMap</h1>;
+    const { lat, lng } = this.props.initialCenter;
+    this.state = {
+      currentLocation: {
+        lat: lat,
+        lng: lng
+      }
+    };
+  }
+  componentDidMount() {
+    if (this.props.centerAroundCurrentLocation) {
+      if (navigator && navigator.geolocation) {
+        const watchID = navigator.geolocation.watchPosition(pos => {
+          const coords = pos.coords;
+          console.log(coords);
+          this.setState({
+            currentLocation: {
+              lat: coords.latitude,
+              lng: coords.longitude
+            }
+          });
+          this.loadMap();
+        });
+      }
+    }
+    this.loadMap();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.google !== this.props.google) {
+      this.loadMap();
+    }
+  }
+
+  loadMap() {
+    if (this.props && this.props.google) {
+      // google is available
+      const { google } = this.props;
+      const maps = google.maps;
+
+      const mapRef = this.refs.map;
+      const node = ReactDOM.findDOMNode(mapRef);
+
+      let { initialCenter, zoom } = this.props;
+      const { lat, lng } = this.state.currentLocation;
+
+      const center = new maps.LatLng(lat, lng);
+      const mapConfig = Object.assign(
+        {},
+        {
+          center: center,
+          zoom: zoom
+        }
+      );
+      this.map = new maps.Map(node, mapConfig);
+    }
+    // ...
+  }
+
+  render() {
+    const style = {
+      width: "100vw",
+      height: "100vh"
+    };
+    return <div style={style} ref="map" />;
+  }
 }
 
-export default GoogleMap;
+Container.defaultProps = {
+  zoom: 13,
+  // San Francisco, by default
+  initialCenter: {
+    lat: 37.774929,
+    lng: -122.419416
+  },
+  centerAroundCurrentLocation: true
+};
 
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyDUGEuJkyJyI45iy3uslI6PUcRw2-CdZQg"
+})(Container);
